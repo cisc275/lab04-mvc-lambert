5d890b73bc512fd6713c35b55b6e8018b36ff1ea
Charles Lambert <clambert@udel.edu>
2019-03-10 13:25:37 -0400

    Fixed accidental addition to Controller.java

--- src/Controller.java
+++ src/Controller.java
@@ -13,7 +13,6 @@ public class Controller {
 	
         //run the simulation
 	public void start(){
-		View.loadWindow();
 		for(int i = 0; i < 5000; i++)
 		{
 			//increment the x and y coordinates, alter direction if necessary
